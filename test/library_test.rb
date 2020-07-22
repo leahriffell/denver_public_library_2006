4e1f68e2cec26624143a16f5e191acd298392440
Leah Riffell <leah.riffell@gmail.com>
2020-07-22 13:42:48 -0600

    Add initialize method for library

---
+++ test/library_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/library'
+require './lib/author'
+
+class LibraryTest < MiniTest::Test
+  def setup
+    @dpl = Library.new("Denver Public Library")
+  end
+
+  def test_it_exists
+    assert_instance_of Library, @dpl
+  end
+end
