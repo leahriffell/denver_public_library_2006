d77a61a32b423a5ec9240d8839b31b10707d56fd
Leah Riffell <leah.riffell@gmail.com>
2020-07-22 13:40:21 -0600

    Attempt write method (revist)

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,14 @@
 class Author
-  attr_reader :name, :books
+  attr_reader :first_name, :last_name, :name, :books
 
   def initialize(attributes = {})
+    @first_name = "#{attributes[:first_name]}"
+    @last_name = "#{attributes[:last_name]}"
     @name = "#{attributes[:first_name]} #{attributes[:last_name]}"
     @books = []
+  end
+
+  def write(title, publication_date)
+    @books << Book.new({author_first_name: @first_name, author_last_name: @last_name, title: title, publication_date: publication_date})
   end
 end
