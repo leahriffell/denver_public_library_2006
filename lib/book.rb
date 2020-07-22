95efcef828a0ff6ac90633a14c55cbf10d9c8ea3
Leah Riffell <leah.riffell@gmail.com>
2020-07-22 12:38:33 -0600

    Add book class and initialize method

---
+++ lib/book.rb
@@ -0,0 +1,8 @@
+class Book
+  def initialize(attributes = {})
+    @author_first_name = attributes[:author_first_name]
+    @author_last_name = attributes[:author_last_name]
+    @title = attributes[:title]
+    @publication_date = attributes[:publication_date]
+  end
+end
