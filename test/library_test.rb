33db8b699152f872e49999bb572b86779230e9f9
Leah Riffell <leah.riffell@gmail.com>
2020-07-22 13:49:03 -0600

    Add test to add authors to library

--- test/library_test.rb
+++ test/library_test.rb
@@ -6,6 +6,12 @@ require './lib/author'
 class LibraryTest < MiniTest::Test
   def setup
     @dpl = Library.new("Denver Public Library")
+    @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    # @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    # @professor = charlotte_bronte.write("The Professor", "1857")
+    # @villette = charlotte_bronte.write("Villette", "1853")
+    @harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    # @mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
   end
 
   def test_it_exists
@@ -16,5 +22,12 @@ class LibraryTest < MiniTest::Test
     assert_equal "Denver Public Library", @dpl.name
     assert_equal [], @dpl.books
     assert_equal [], @dpl.authors
+  end
+
+  def test_it_can_add_authors
+    @dpl.add_author(@charlotte_bronte)
+    assert_equal [@charlotte_bronte], @dpl.authors
+    @dpl.add_author(@harper_lee)
+    assert_equal [@charlotte_bronte, @harper_lee], @dpl.authors
   end
 end
