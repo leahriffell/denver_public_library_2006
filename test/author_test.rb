41b0b4612abf7ab9145b5d7a0242fe706d0324bc
Leah Riffell <leah.riffell@gmail.com>
2020-07-22 12:57:03 -0600

    Add test that author can write books

--- test/author_test.rb
+++ test/author_test.rb
@@ -14,6 +14,18 @@ class AuthorTest < MiniTest::Test
 
   def test_it_has_readable_attributes
     assert_equal "Charlotte Bronte", @charlotte_bronte.name
+  end
+
+  def test_it_can_write_books
     assert_equal [], @charlotte_bronte.books
+
+    @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    assert_equal [@jane_eyre], @charlotte_bronte.books
+
+    assert_instance_of Book, @jane_eyre
+    assert_equal "Jane Eyre", @jane_eyre.title
+
+    @charlotte_bronte.write("Villette", "1853")
+    assert_equal [@jane_eyre, villette]
   end
 end
