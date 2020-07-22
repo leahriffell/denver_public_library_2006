135eec7bd44ab1df1e9c4a7839cc5caf741666ae
Leah Riffell <leah.riffell@gmail.com>
2020-07-22 12:45:54 -0600

    Add initialize test for author

---
+++ test/author_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/book'
+require './lib/author'
+
+class AuthorTest < MiniTest::Test
+  def setup
+    @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+  end
+
+  def test_it_exists
+    assert_instance_of Author, @charlotte_bronte
+  end
+end
