4109a2c57f39a5f4c0cea0a2acb74898309a89d7
Leah Riffell <leah.riffell@gmail.com>
2020-07-23 09:20:29 -0600

    Remove unnecessary variable declarations

--- lib/author.rb
+++ lib/author.rb
@@ -4,15 +4,14 @@ class Author
   attr_reader :first_name, :last_name, :name, :books
 
   def initialize(attributes = {})
-    @first_name = "#{attributes[:first_name]}"
-    @last_name = "#{attributes[:last_name]}"
+  # don't have to do this syntax. Can just do attributes withouth = {}
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
     @name = "#{attributes[:first_name]} #{attributes[:last_name]}"
     @books = []
   end
 
   def write(title, publication_date)
-    author_first_name = @first_name
-    author_last_name = @last_name
     @books << Book.create_book(first_name, last_name, title, publication_date)
 
     # so funky but I couldn't figure out how to return book object instead of @books array
